a58175591d94ecc8cb320051948eae56ffe9e5fd
anjaneesh pandey <4648anjaneeshpandey9c@gmail.com>
2026-02-24 09:46:19 +0530

    Update index.html and script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello, World! This is a simple JavaScript file.");
+//console.log("Hello, World! This is a simple JavaScript file.");
+var num=10;
+    console.log(num);
+    console.log(typeof(num));
+    num=false;
+    console.log(num);
+    console.log(typeof(num));
+    num="Hello, World!";
+    console.log(num);
+    console.log(typeof num);
+    var sayHello=function(){
+        console.log("Hello, World!");
+    }
+    var numberarray=[1,2,3,4,5];
+    var animals=["cat","dog","rabbit","tiger"];
+    var person={
+        name:"John",
+        age:30,
+        title:"Software Engineer",
+    }   
+    
