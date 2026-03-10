73765d801431d10f191a8eaa0fbe20725586f8b4
anjaneesh pandey <4648anjaneeshpandey9c@gmail.com>
2026-03-10 09:21:06 +0530

    #4

---
+++ arithmeticop.js
@@ -0,0 +1,8 @@
+arithmeticop.js
+var count;
+document.write("Starting Loop"+"<br/>");
+for(count=0;count<10;count++){
+    document.write("Current Count:"+count);
+    document.write("<br/>");
+}
+document.write("Loop stopped!");
