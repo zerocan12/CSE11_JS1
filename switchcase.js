6248edb2d14038d6c18c375dd5964aa3aa88ddab
anjaneesh pandey <4648anjaneeshpandey9c@gmail.com>
2026-03-10 09:23:11 +0530

    #5

---
+++ switchcase.js
@@ -0,0 +1,21 @@
+switchcase.js
+var grade='A';
+switch(grade){
+    case 'A':
+        console.log("Excellent!");
+        break;
+    case 'B':
+        console.log("Good job!");
+        break;
+    case 'C':
+        console.log("Average.");
+        break;
+    case 'D':
+        console.log("Needs improvement.");
+        break;
+    case 'F':
+        console.log("Failing grade.");
+        break;
+    default:
+        console.log("Invalid grad");
+}
