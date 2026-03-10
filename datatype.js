752d52d99eac14ebaa5076433dcc757ece1282b8
anjaneesh pandey <4648anjaneeshpandey9c@gmail.com>
2026-03-10 09:16:04 +0530

    #2

---
+++ datatype.js
@@ -0,0 +1,10 @@
+datatype.js
+var num =10;
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
